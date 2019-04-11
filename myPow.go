34322d87e2056444ffbaca4d4e165107fc3b5f47
Chris Felli <v-chfell@microsoft.com>
2019-04-11 13:47:18 -0700

    myPow

---
+++ myPow.go
@@ -0,0 +1,13 @@
+/*
+CHRIS FELLI, 2019
+Calculate x^n
+*/
+
+//Package gocode ...
+package gocode
+
+import "math"
+
+func myPow(x float64, n int) float64 {
+	return math.Pow(x, float64(n))
+}
---
+++ myPow_test.go
@@ -0,0 +1,23 @@
+/*
+CHRIS FELLI, 2019
+Calculate x^n
+*/
+
+//Package gocode ...
+package gocode
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestMyPow1(t *testing.T) {
+	got := myPow(2, 10)
+	want := 1024.0
+
+	if got != want {
+		t.Errorf("Got %f, wanted %f\nFAILED!\n", got, want)
+	} else {
+		fmt.Printf("Got %f, wanted %f\nPASSED!\n\n", got, want)
+	}
+}
